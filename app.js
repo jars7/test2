f09981f3296ab0356e8b0fae89c07456c7fe2531
jars7 <jl@gcd.my>
2017-10-16 14:13:58 +0800

    3rd commit

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 const express = require('express');
-const bodyPraser = require('body-parser');
+const bodyParser = require('body-parser');
 const exphbs = require('express-handlebars');
-
+const path = require('path');
 const app = express();
 
+// View engine setup
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+// static folder
+app.use('/public',express.static(path.join(__dirname, 'public')));
+
+
+// Body Parser setup
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 app.get('/', (req, res)=> {
-    res.send("hello world yo")
+    res.render('contact');
 })
 
 app.listen(8000);
